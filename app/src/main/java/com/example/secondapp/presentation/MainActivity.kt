c33de268ac25b377a91de6d18839aac366e67d9f
Anudeep <anudeep.22cs@kct.ac.in>
2023-07-19 21:53:37 +0530

    add two new screens

--- app/src/main/java/com/example/secondapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/secondapp/presentation/MainActivity.kt
@@ -13,25 +13,19 @@ import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.paddingFrom
-import androidx.compose.foundation.layout.paddingFromBaseline
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.wear.compose.material.Button
@@ -39,49 +33,58 @@ import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
 import com.example.secondapp.R
 import com.example.secondapp.presentation.theme.SecondAppTheme
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.wear.compose.material.ButtonDefaults
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.wear.compose.material.SwipeToDismissBox
-import androidx.wear.compose.material.SwipeToDismissBoxDefaults
-import androidx.wear.compose.material.SwipeableState
-import androidx.wear.compose.material.rememberSwipeableState
-import androidx.wear.compose.material.SwipeToDismissValue
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        requestPermissions(arrayOf(ComponentActivity.SENSOR_SERVICE), ComponentActivity.BIND_IMPORTANT)
         setContent {
-            SensorPermissionScreen {
-
-            }
+            ShowPage()
+        //            ShowPage()
         }
+
     }
 
 }
 
+
+/*
+* * * Page navigating function
+*/
 @Composable
 fun ShowPage(){
-    var page by rememberSaveable { mutableStateOf(1) }
+    var page by rememberSaveable { mutableStateOf(2) }
 
     when(page){
-        1 -> GetStarted(click = {page = 2})
-        2 -> StartPage()
+        2 -> GetStarted(click = {page = 3})
+        3 -> StartPage(click = { page = 5})
+//        4 -> SensorPermissionScreen{}
+        5 -> InfoPage(click = { page = 6})
+        6 -> HomePage()
+
+
     }
 
 }
 
+/*
+* * *  Get started page, page no.2
+*/
 @Composable
 fun GetStarted(click:() -> Unit) {
     SecondAppTheme {
@@ -122,8 +125,12 @@ fun GetStarted(click:() -> Unit) {
 
 }
 
+
+/*
+* * *  start page, page no.3
+*/
 @Composable
-fun StartPage() {
+fun StartPage(click: () -> Unit) {
     SecondAppTheme {
 
         Column(modifier = Modifier
@@ -150,7 +157,7 @@ fun StartPage() {
                     .background(MaterialTheme.colors.background)
                     .padding(top = 5.dp),
             ) {
-                Button(onClick = {  }, colors= ButtonDefaults.buttonColors(backgroundColor = Color.White),modifier = Modifier
+                Button(onClick = {click() }, colors= ButtonDefaults.buttonColors(backgroundColor = Color.White),modifier = Modifier
                     .height(20.dp)
                     .padding(start = 84.dp)) {
                     Text(text = "Start", fontSize = 8.sp, color = Color.Black)
@@ -165,6 +172,88 @@ fun StartPage() {
 
 }
 
+
+/*
+* * *  Info  page, page no.5
+*/
+@Composable
+fun InfoPage(click: () -> Unit) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Row() {
+            Text(
+                text = "What is normal Apnea?",
+                color = Color.White, fontSize = 12.sp)
+        }
+        Row(){
+            Text(
+                text = "0 - 5 -> Normal\n5 - 15 -> Mild\n15 - 31 -> Moderate\nAbove 31 -> Severe",
+                color = Color.White,
+                fontSize = 11.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(top = 10.dp),
+                lineHeight = 16.sp
+            )
+        }
+
+        Row( modifier = Modifier.padding(top=15.dp)){
+        Button(onClick = { click()  }, colors= ButtonDefaults.buttonColors(backgroundColor = Color.White),modifier = Modifier
+            .height(20.dp)
+            .padding(start = 55.dp)) {
+            Text(text = "Get started", fontSize = 8.sp, color = Color.Black)
+        }
+
+        }
+
+    }
+
+}
+
+
+/*
+* * *  Home page, page no.6
+*/
+@Composable
+fun HomePage(){
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            modifier = Modifier.padding(bottom=5.dp),
+        ){
+            Text(text = "Hello user", fontSize = 13.sp)
+        }
+        Row(
+            modifier = Modifier
+                .width(90.dp)
+                .height(90.dp),
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.img),
+                contentDescription =  stringResource(id = R.string.tick)
+            )
+
+        }
+        Text(text = "You are all good", color = Color.White, fontSize = 9.sp)
+        Row(){
+            Button(onClick = { /*TODO*/ },colors= ButtonDefaults.buttonColors(backgroundColor = Color.White),modifier = Modifier.height(20.dp)) {
+                Text(text = "More Details", fontSize = 8.sp, color = Color.Black)
+            }
+        }
+    }
+}
+
+
+
+/*
+* * * Permission section, page no. 5
+*/
+
 @Composable
 fun SensorPermissionScreen(
     onPermissionGranted: () -> Unit,
@@ -174,6 +263,7 @@ fun SensorPermissionScreen(
     val snackbarVisibleState = remember { mutableStateOf(false) }
 
     Column {
+
         Text(
             text = "This app requires sensor permission.",
             style = MaterialTheme.typography.caption2,
@@ -210,6 +300,8 @@ fun SensorPermissionScreen(
 //    }
 }
 
+
+
 private fun hasSensorPermission(context: Context): Boolean {
     val permission = Manifest.permission.BODY_SENSORS
     val result = ContextCompat.checkSelfPermission(context, permission)
@@ -224,8 +316,10 @@ private fun requestSensorPermission(context: Context) {
 @Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true, showBackground = true)
 @Composable
 fun DefaultPreview() {
-    GetStarted() {}
+//    GetStarted() {}
 //    startPage()
 //    SensorPermissionScreen {}
-
+//    InfoPage()
+//    ShowPage()
+    HomePage()
 }
